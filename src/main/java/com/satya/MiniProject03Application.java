5bd836deff863bdd93ca9a1946150002149b73e5
nayaksatya1232 <nayaksatya1232@gmail.com>
2023-05-11 14:49:38 +0530

    Password Encryption Implemented

--- src/main/java/com/satya/MiniProject03Application.java
+++ src/main/java/com/satya/MiniProject03Application.java
@@ -2,10 +2,17 @@ package com.satya;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
+@Configuration
 public class MiniProject03Application {
-
+	@Bean
+	BCryptPasswordEncoder getEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(MiniProject03Application.class, args);
 	}
